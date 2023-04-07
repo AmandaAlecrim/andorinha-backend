a915b233f5d017441e43700ad3b202f6dbadc2d4
Amanda Demétrio Alecrim <amanda2668@hotmail.com>
2023-04-07 19:30:29 -0300

    adicionando base dos repositórios e testes

---
+++ src/main/java/repository/ComentarioRepository.java
@@ -0,0 +1,27 @@
+package repository;
+
+import java.util.List;
+
+import model.Comentario;
+public class ComentarioRepository extends AbstractCrudRepository{
+	
+public void inserir(Comentario comentario) {
+		
+	}
+	
+	public Comentario consultar(int id) {
+		return null;		
+	}
+	
+	public List<Comentario> listarTodos() {
+		return null;		
+	}
+	
+	public void atualizar (Comentario comentario) {
+		
+	}
+	
+	public void remover (int id) {
+		
+	}
+}
---
+++ src/main/java/repository/TweetRepository.java
@@ -0,0 +1,28 @@
+package repository;
+
+import java.util.List;
+
+import model.Tweet;
+
+public class TweetRepository extends AbstractCrudRepository {
+
+	public void inserir(Tweet tweet) {
+		
+	}
+	
+	public Tweet consultar(int id) {
+		return null;		
+	}
+	
+	public List<Tweet> listarTodos() {
+		return null;		
+	}
+	
+	public void atualizar (Tweet tweet) {
+		
+	}
+	
+	public void remover (int id) {
+		
+	}
+}
