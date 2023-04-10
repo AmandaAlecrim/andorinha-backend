b7a0eab97dbcd050ee10c8c234cf001516bd8a71
Amanda Demétrio Alecrim <amanda2668@hotmail.com>
2023-04-10 15:56:13 -0300

    criando CRUD de usuário e testes do mesmo

--- src/main/java/repository/UsuarioRepository.java
+++ src/main/java/repository/UsuarioRepository.java
@@ -1,43 +1,62 @@
 package repository;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import model.Usuario;
 import model.exceptions.ErroAoConectarNaBaseException;
 import model.exceptions.ErroAoConsultarBaseException;
 
-public class UsuarioRepository extends AbstractCrudRepository{
-	
+public class UsuarioRepository extends AbstractCrudRepository {
+
 	public void inserir(Usuario usuario) throws ErroAoConectarNaBaseException, ErroAoConsultarBaseException {
-		
-		//abrir uma conexão com o banco
-		try ( Connection c = this.abrirConexao() ){
-			
-			//próximo valor da sequence
+
+		// abrir uma conexão com o banco
+		try (Connection c = this.abrirConexao()) {
+
+			// próximo valor da sequence
 			int id = this.recuperarProximoValorDaSequence("seq_usuario");
 			usuario.setId(id);
-			
-			//criar e executar o sql
+
+			// criar e executar o sql
 			PreparedStatement ps = c.prepareStatement("insert into usuario (id, nome) values (?, ?)");
 			ps.setInt(1, usuario.getId());
 			ps.setString(2, usuario.getNome());
-			ps.execute();			
+			ps.execute();
 			ps.close();
-			
+
 		} catch (SQLException e) {
 			throw new ErroAoConsultarBaseException("Ocorreu um erro ao inserir usuário", e);
 		}
 	}
-	public void atualizar(Usuario usuario) {
-		
+
+	public void atualizar(Usuario usuario) throws ErroAoConectarNaBaseException, ErroAoConsultarBaseException {
+		try (Connection c = this.abrirConexao()) {
+			PreparedStatement ps = c.prepareStatement("update usuario set nome = ? where id = ?");
+			ps.setString(1, usuario.getNome());
+			ps.setInt(2, usuario.getId());
+			ps.execute();
+			ps.close();
+		} catch (SQLException e) {
+			throw new ErroAoConsultarBaseException("Ocorreu um erro ao atualizar usuário", e);
+		}
 	}
-	public void remover(int id) {
-		
+
+	public void remover(int id) throws ErroAoConectarNaBaseException, ErroAoConsultarBaseException {
+		try (Connection c = this.abrirConexao()) {
+
+			PreparedStatement ps = c.prepareStatement("delete from usuario where id = ?");
+			ps.setInt(1, id);
+			ps.execute();
+			ps.close();
+
+		} catch (SQLException e) {
+			throw new ErroAoConsultarBaseException("Ocorreu um erro ao remover o usuário", e);
+		}
 	}
 
 	public Usuario consultar(int id) throws ErroAoConectarNaBaseException, ErroAoConsultarBaseException {
@@ -64,7 +83,28 @@ public class UsuarioRepository extends AbstractCrudRepository{
 			throw new ErroAoConsultarBaseException("Ocorreu um erro ao consultar usuário", e);
 		}
 	}
-	public List<Usuario> listarTodos() {
-		return null;
+
+	public List<Usuario> listarTodos() throws ErroAoConectarNaBaseException, ErroAoConsultarBaseException {
+		try (Connection c = this.abrirConexao()) {
+
+			List<Usuario> users = new ArrayList<>();
+
+			PreparedStatement ps = c.prepareStatement("select * from usuario");
+			ResultSet rs = ps.executeQuery();
+
+			while (rs.next()) {
+				Usuario user = new Usuario();
+				user.setId(rs.getInt("id"));
+				user.setNome(rs.getString("nome"));
+
+				users.add(user);
+			}
+			rs.close();
+			ps.close();
+
+			return users;
+		} catch (SQLException e) {
+			throw new ErroAoConsultarBaseException("Ocorreu um erro ao listar todos os usuários", e);
+		}
 	}
 }
--- src/test/java/repository/TestUsuarioRepository.java
+++ src/test/java/repository/TestUsuarioRepository.java
@@ -2,6 +2,8 @@ package repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,14 +15,14 @@ import runner.AndorinhaTestRunner;
 
 @RunWith(AndorinhaTestRunner.class)
 public class TestUsuarioRepository {
-	
+
 	private UsuarioRepository usuarioRepository;
-	
+
 	@Before
 	public void setUp() {
 		this.usuarioRepository = new UsuarioRepository();
 	}
-	
+
 	@Test
 	public void testa_se_usuario_foi_inserido() throws ErroAoConectarNaBaseException, ErroAoConsultarBaseException {
 		Usuario user = new Usuario();
@@ -28,23 +30,54 @@ public class TestUsuarioRepository {
 		this.usuarioRepository.inserir(user);
 
 		Usuario inserido = this.usuarioRepository.consultar(user.getId());
-		
-		assertThat(user.getId() ).isGreaterThan(0);
-		
+
+		assertThat(user.getId()).isGreaterThan(0);
+
 		assertThat(inserido).isNotNull();
 		assertThat(inserido.getNome()).isEqualTo(user.getNome());
 		assertThat(inserido.getId()).isEqualTo(user.getId());
-		
+
 	}
+
 	@Test
 	public void testa_consultar_usuario() throws ErroAoConsultarBaseException, ErroAoConectarNaBaseException {
-		
+
 		int idConsulta = 6;
 		Usuario user = this.usuarioRepository.consultar(idConsulta);
-		
+
 		assertThat(user).isNotNull();
 		assertThat(user.getNome()).isEqualTo("Usuario do Teste de unidade");
 		assertThat(user.getId()).isEqualTo(idConsulta);
+	}
+
+	@Test
+	public void testa_alterar_usuario() throws ErroAoConectarNaBaseException, ErroAoConsultarBaseException {
+		int idConsulta = 5;
+		Usuario user = this.usuarioRepository.consultar(idConsulta);
+		user.setNome("Nome alterado");
+		this.usuarioRepository.atualizar(user);
+		Usuario alterado = this.usuarioRepository.consultar(idConsulta);
+
+		assertThat(alterado).isEqualToComparingFieldByField(user);
+	}
+
+	@Test
+	public void testa_remover_usuario() throws ErroAoConectarNaBaseException, ErroAoConsultarBaseException {
+		int idConsulta = 6;
+		Usuario user = this.usuarioRepository.consultar(idConsulta);
+		assertThat(user).isNotNull();
+
+		this.usuarioRepository.remover(idConsulta);
+		Usuario removido = this.usuarioRepository.consultar(idConsulta);
+		assertThat(removido).isNull();
+
+	}
+
+	@Test
+	public void testa_listar_todos_usuarios() throws ErroAoConectarNaBaseException, ErroAoConsultarBaseException {
+		List<Usuario> usuarios = this.usuarioRepository.listarTodos();
+
+		assertThat(usuarios).isNotNull().isNotEmpty();
 	}
 
 }
