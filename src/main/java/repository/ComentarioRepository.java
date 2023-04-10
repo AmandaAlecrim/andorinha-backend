e36703e1cc5717239cb30ee8e0a217456f840124
Amanda Demétrio Alecrim <amanda2668@hotmail.com>
2023-04-10 20:17:54 -0300

    criando CRUD de comentário e testes do mesmo

--- src/main/java/repository/ComentarioRepository.java
+++ src/main/java/repository/ComentarioRepository.java
@@ -1,27 +1,183 @@
 package repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
+import java.util.ArrayList;
 
 import model.Comentario;
+import model.Usuario;
+import model.Tweet;
+import model.exceptions.ErroAoConectarNaBaseException;
+import model.exceptions.ErroAoConsultarBaseException;
 public class ComentarioRepository extends AbstractCrudRepository{
 	
-public void inserir(Comentario comentario) {
+public void inserir(Comentario comentario) throws ErroAoConectarNaBaseException, ErroAoConsultarBaseException {
+	try (Connection c = this.abrirConexao()) {
+
+		int id = this.recuperarProximoValorDaSequence("seq_comentario");
+		comentario.setId(id);
 		
-	}
-	
-	public Comentario consultar(int id) {
-		return null;		
-	}
-	
-	public List<Comentario> listarTodos() {
-		return null;		
-	}
-	
-	public void atualizar (Comentario comentario) {
+		Calendar hoje = Calendar.getInstance();
 		
+		PreparedStatement ps = c.prepareStatement("insert into comentario (id, conteudo, data_postagem, id_usuario, id_tweet) values (?, ?, ?, ?, ?)");
+		ps.setInt(1, id);
+		ps.setString(2, comentario.getConteudo());
+		ps.setTimestamp(3, new Timestamp(hoje.getTimeInMillis()));
+		ps.setInt(4, comentario.getUsuario().getId());
+		ps.setInt(5, comentario.getTweet().getId());
+		ps.execute();
+		ps.close();
+		
+	} catch (SQLException e) {
+		throw new ErroAoConsultarBaseException("Ocorreu um erro ao inserir o comentário", e);
+	}
 	}
 	
-	public void remover (int id) {
-		
+	public Comentario consultar(int id) throws ErroAoConectarNaBaseException, ErroAoConsultarBaseException {
+		try (Connection c = this.abrirConexao()) {
+			
+			Comentario comentario = null;
+			
+			StringBuilder sql = new StringBuilder();
+			sql.append("select c.id, c.conteudo, c.data_postagem, c.id_usuario, c.id_tweet, ");
+			sql.append("u.nome as nome_usuario, t.conteudo as conteudo_tweet, t.data_postagem as data_postagem_tweet, ");
+			sql.append("t.id_usuario as id_usuario_tweet, ut.nome as nome_usuario_tweet ");
+			sql.append("from comentario c ");
+			sql.append("join tweet t on c.id_tweet = t.id ");
+			sql.append("join usuario u on c.id_usuario = u.id ");
+			sql.append("join usuario ut on t.id_usuario = ut.id ");
+			sql.append("where c.id = ? ");
+			
+			PreparedStatement ps = c.prepareStatement(sql.toString());
+			ps.setInt(1, id);
+			ResultSet rs = ps.executeQuery();
+			
+			if (rs.next()) {
+				Calendar dataTweet = new GregorianCalendar();
+				dataTweet.setTime(rs.getTimestamp("data_postagem_tweet"));
+				
+				Calendar dataComentario = new GregorianCalendar();
+				dataTweet.setTime(rs.getTimestamp("data_postagem"));
+				
+				Usuario userTweet = new Usuario();
+				userTweet.setId(rs.getInt("id_usuario_tweet"));
+				userTweet.setNome(rs.getString("nome_usuario_tweet"));
+				
+				Usuario userComentario = new Usuario();
+				userComentario.setId(rs.getInt("id_usuario"));
+				userComentario.setNome(rs.getString("nome_usuario"));
+				
+				Tweet tweet = new Tweet();
+				tweet.setId(rs.getInt("id_tweet"));
+				tweet.setTexto(rs.getString("conteudo_tweet"));
+				tweet.setData(dataTweet);
+				tweet.setUsuario(userTweet);
+				
+				comentario = new Comentario();
+				comentario.setId(rs.getInt("id"));
+				comentario.setConteudo(rs.getString("conteudo"));
+				comentario.setData(dataComentario);
+				comentario.setTweet(tweet);
+				comentario.setUsuario(userComentario);				
+			}
+			rs.close();
+			ps.close();
+			
+			return comentario;
+		} catch (SQLException e) {
+			throw new ErroAoConsultarBaseException("Ocorreu um erro ao consultar o comentário", e);
+		}	
+	}
+	
+	public List<Comentario> listarTodos() throws ErroAoConectarNaBaseException, ErroAoConsultarBaseException {
+		try (Connection c = this.abrirConexao()) {
+			
+			List<Comentario> comentarios = new ArrayList<>();
+			
+			StringBuilder sql = new StringBuilder();
+			sql.append("select c.id, c.conteudo, c.data_postagem, c.id_usuario, c.id_tweet, ");
+			sql.append("u.nome as nome_usuario, t.conteudo as conteudo_tweet, t.data_postagem as data_postagem_tweet, ");
+			sql.append("t.id_usuario as id_usuario_tweet, ut.nome as nome_usuario_tweet ");
+			sql.append("from comentario c ");
+			sql.append("join tweet t on c.id_tweet = t.id ");
+			sql.append("join usuario u on c.id_usuario = u.id ");
+			sql.append("join usuario ut on t.id_usuario = ut.id ");
+			
+			PreparedStatement ps = c.prepareStatement(sql.toString());
+			ResultSet rs = ps.executeQuery();
+			
+			while (rs.next()) {
+				Calendar dataTweet = new GregorianCalendar();
+				dataTweet.setTime(rs.getTimestamp("data_postagem_tweet"));
+				
+				Calendar dataComentario = new GregorianCalendar();
+				dataComentario.setTime(rs.getTimestamp("data_postagem"));
+				
+				Usuario userTweet = new Usuario();
+				userTweet.setId(rs.getInt("id_usuario_tweet"));
+				userTweet.setNome(rs.getString("nome_usuario_tweet"));
+				
+				Usuario userComentario = new Usuario();
+				userComentario.setId(rs.getInt("id_usuario"));
+				userComentario.setNome(rs.getString("nome_usuario"));
+				
+				Tweet tweet = new Tweet();
+				tweet.setId(rs.getInt("id_tweet"));
+				tweet.setTexto(rs.getString("conteudo_tweet"));
+				tweet.setData(dataTweet);
+				tweet.setUsuario(userTweet);
+				
+				Comentario comentario = new Comentario();
+				comentario.setId(rs.getInt("id"));
+				comentario.setConteudo(rs.getString("conteudo"));
+				comentario.setData(dataComentario);
+				comentario.setTweet(tweet);
+				comentario.setUsuario(userComentario);
+				
+				comentarios.add(comentario);
+			}
+			rs.close();
+			ps.close();
+			
+			return comentarios;
+		} catch (SQLException e) {
+			throw new ErroAoConsultarBaseException("Ocorreu um erro ao listar todos os comentários", e);
+		}		
+	}
+	
+	public void atualizar (Comentario comentario) throws ErroAoConectarNaBaseException, ErroAoConsultarBaseException {
+		try (Connection c = this.abrirConexao()) {
+			
+			Calendar hoje = Calendar.getInstance();
+			
+			PreparedStatement ps = c.prepareStatement("update comentario set conteudo = ?, data_postagem = ? where id = ?");
+			ps.setString(1, comentario.getConteudo());
+			ps.setTimestamp(2, new Timestamp(hoje.getTimeInMillis()));
+			ps.setInt(3, comentario.getId());
+			ps.execute();
+			ps.close();
+			
+		} catch (SQLException e) {
+			throw new ErroAoConsultarBaseException("Ocorreu um erro ao atualizar o comentário", e);
+		}
+	}
+	
+	public void remover (int id) throws ErroAoConectarNaBaseException, ErroAoConsultarBaseException {
+		try (Connection c = this.abrirConexao()) {
+			
+			PreparedStatement ps = c.prepareStatement("delete from comentario where id = ?");
+			ps.setInt(1, id);
+			ps.execute();
+			ps.close();
+			
+		} catch (SQLException e) {
+			throw new ErroAoConsultarBaseException("Ocorreu um erro ao remover o comentário", e);
+		}
 	}
 }
--- src/test/java/repository/TestComentarioRepository.java
+++ src/test/java/repository/TestComentarioRepository.java
@@ -1,5 +1,102 @@
 package repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import model.Comentario;
+import model.Tweet;
+import model.Usuario;
+import model.exceptions.ErroAoConectarNaBaseException;
+import model.exceptions.ErroAoConsultarBaseException;
+
 public class TestComentarioRepository {
 
+	private UsuarioRepository usuarioRepository;
+	private TweetRepository tweetRepository;
+	private ComentarioRepository comentarioRepository; 
+
+	@Before
+	public void setUp() {
+		this.usuarioRepository = new UsuarioRepository();
+		this.tweetRepository = new TweetRepository();
+		this.comentarioRepository = new ComentarioRepository();
+	}
+
+	@Test
+	public void test_consultar_comentario() throws ErroAoConsultarBaseException, ErroAoConectarNaBaseException {
+		int idConsulta = 2;
+		Comentario comentario = this.comentarioRepository.consultar(idConsulta);
+
+		assertThat(comentario).isNotNull();
+		assertThat(comentario.getConteudo()).isEqualTo("Comentário 2");
+		assertThat(comentario.getId()).isEqualTo(idConsulta);
+		assertThat(comentario.getUsuario()).isNotNull();
+		assertThat(comentario.getTweet()).isNotNull();
+		assertThat(comentario.getTweet().getUsuario()).isNotNull();
+	}
+
+	@Test
+	public void testa_se_comentario_foi_inserido() throws ErroAoConectarNaBaseException, ErroAoConsultarBaseException {
+
+		int idUsuarioConsulta = 2;
+		int idTweetConsulta = 9;
+
+		Usuario user = this.usuarioRepository.consultar(idUsuarioConsulta);
+		Tweet tweet = this.tweetRepository.consultar(idTweetConsulta);
+
+		Comentario comentario = new Comentario();
+
+		comentario.setConteudo("Comentario teste");
+		comentario.setUsuario(user);
+		comentario.setTweet(tweet);
+		
+		this.comentarioRepository.inserir(comentario);
+
+		assertThat(comentario.getId()).isGreaterThan(0);
+
+		Comentario inserido = this.comentarioRepository.consultar(comentario.getId());
+
+		assertThat(inserido).isNotNull();
+		assertThat(inserido.getConteudo()).isEqualTo(comentario.getConteudo());
+
+	}
+	
+	@Test
+	public void test_listar_todos_comentarios() throws ErroAoConectarNaBaseException, ErroAoConsultarBaseException {
+		List<Comentario> comentarios = this.comentarioRepository.listarTodos();
+		
+		assertThat(comentarios).isNotNull().isNotEmpty();
+	}
+	
+	@Test
+	public void testa_alterar_comentario() throws ErroAoConectarNaBaseException, ErroAoConsultarBaseException {
+		int idComentario = 11;
+		
+		Comentario c = this.comentarioRepository.consultar(idComentario);
+		c.setConteudo("Comentário alterado");
+		
+		this.comentarioRepository.atualizar(c);
+		
+		Comentario alterado = this.comentarioRepository.consultar(idComentario);
+		
+		assertThat(alterado.getConteudo()).isEqualTo(c.getConteudo());
+	}
+	
+	@Test
+	public void testa_remover_comenatrio() throws ErroAoConectarNaBaseException, ErroAoConsultarBaseException {
+		int idComentario = 11;
+		
+		Comentario c = this.comentarioRepository.consultar(idComentario);
+		assertThat(c).isNotNull();
+		
+		this.comentarioRepository.remover(idComentario);
+		
+		Comentario removido = this.comentarioRepository.consultar(idComentario);
+		assertThat(removido).isNull();
+	}
+	
 }
